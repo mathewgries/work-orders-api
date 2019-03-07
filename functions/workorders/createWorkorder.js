4edae1c86e12eb80b8a0e11da47db77eadd3615d
mathewgries <mathewgries@gmail.com>
2019-03-06 20:35:28 -0500

    Building out API functions and tables

--- functions/workorders/createWorkorder.js
+++ functions/workorders/createWorkorder.js
@@ -1,10 +1,11 @@
 import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const { content } = data
+  
   const params = {
     TableName: process.env.tableName,
     Item: {
