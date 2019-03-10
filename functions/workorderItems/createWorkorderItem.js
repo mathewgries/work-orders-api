6162384422fc4c98b8462a107b56ea99b443a195
mathewgries <mathewgries@gmail.com>
2019-03-10 10:06:38 -0400

    Move total operation to front end

--- functions/workorderItems/createWorkorderItem.js
+++ functions/workorderItems/createWorkorderItem.js
@@ -1,12 +1,9 @@
 import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
-import { currencyRounder } from '../helpers/currencyRounder'
 
 export async function main(event, context){
     const data = JSON.parse(event.body)
     const { content } = data
-    
-    const price = currencyRounder(content.quanity, content.unitPrice)
     
     const params = {
         TableName: process.env.workordersItemsTable,
@@ -19,7 +16,7 @@ export async function main(event, context){
             description: content.description,
             quanity: content.quanity,
             unitPrice: content.unitPrice,
-            total: price,
+            total: content.total,
             createdAt: Date.now()
         }
     }
