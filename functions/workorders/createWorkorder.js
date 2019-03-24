2d1fa31a78627a2535c19db6311ee5aa53a95167
mathewgries <mathewgries@gmail.com>
2019-03-24 14:54:03 -0400

    Figuring out forms part 2

--- functions/workorders/createWorkorder.js
+++ functions/workorders/createWorkorder.js
@@ -1,24 +1,26 @@
 import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
+import uuid from 'uuid'
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
-  const { content } = data
+	const data = JSON.parse(event.body);
+	const { content } = data
 
-  const params = {
-    TableName: process.env.workordersTable,
-    Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      title: content.title,
-      description: content.description,
-      createdAt: Date.now()
-    }
-  };
+	const params = {
+		TableName: process.env.workordersTable,
+		Item: {
+			workorderId: uuid.v1(),
+			userId: event.requestContext.identity.cognitoIdentityId,
+			title: content.title,
+			description: content.description,
+			createdAt: Date.now()
+		}
+	};
 
-  try {
-    await dynamoDbLib.call("put", params);
-    return success(params.Item);
-  } catch (e) {
-    return failure({ status: false });
-  }
+	try {
+		await dynamoDbLib.call("put", params);
+		return success(params.Item);
+	} catch (e) {
+		return failure({ status: false });
+	}
 }
