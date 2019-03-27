cf1eaca3a2e38084f9974ad07f21676f5adbf36c
mathewgries <mathewgries@gmail.com>
2019-03-26 21:26:08 -0400

    Added id creation back in createClient

--- functions/clients/createClient.js
+++ functions/clients/createClient.js
@@ -1,5 +1,6 @@
 import * as dynamodbLib from '../../libs/dynamodb-lib'
 import { success, failure } from '../../libs/response-lib'
+import uuid from 'uuid'
 
 export async function main(event, context) {
     const data = JSON.parse(event.body)
@@ -8,6 +9,7 @@ export async function main(event, context) {
     const params = {
         TableName: process.env.workordersClientsTable,
         Item: {
+            clientId: uuid.v1(),
             userId: event.requestContext.identity.cognitoIdentityId,
             name: content.name,
             type: content.type || null, // Residential or Commercial
