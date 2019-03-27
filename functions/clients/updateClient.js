bfc01bb7dac882b29480e740d7a9dee13c9034d5
mathewgries <mathewgries@gmail.com>
2019-03-26 23:37:53 -0400

    Update updateClient part 2

--- functions/clients/updateClient.js
+++ functions/clients/updateClient.js
@@ -3,22 +3,21 @@ import { success, failure } from '../../libs/response-lib'
 
 export async function main(event, context) {
     const data = JSON.parse(event.body)
-    const { content } = data
 
     const params = {
         TableName: process.env.workordersClientsTable,
         Key: {
-            clientId: event.pathParameters.id,
             userId: event.requestContext.identity.cognitoIdentityId,
+			clientId: event.pathParameters.id
         },
         UpdateExpression: `SET 
             name = :name,
             type = :type,
             email = :email`,
         ExpressionAttributeValues: {
-            ":name": content.name,
-            ":type": content.type,
-            ":email": content.email
+            ":name": data.name,
+            ":type": data.type,
+            ":email": data.email
         },
         ReturnValue: "ALL_NEW"
     }
--- functions/workorders/updateWorkorder.js
+++ functions/workorders/updateWorkorder.js
@@ -2,38 +2,38 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
-  const params = {
-    TableName: process.env.workordersTable,
-    // 'Key' defines the partition key and sort key of the item to be updated
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'workorderId': path parameter
-    Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      workorderId: event.pathParameters.id
-    },
-    // 'UpdateExpression' defines the attributes to be updated
-    // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: `SET 
-		title = :title, 
-		clientId = :clientId, 
-		contactId = :contactId, 
-		description = :description, 
-		modifiedAt = :modifiedAt`,
-    ExpressionAttributeValues: {
-		":title": data.title || null,
-		":clientId": data.clientId || null,
-		":contactId": data.contactId || null,
-		":description": data.description || null,
-		":modifiedAt": Date.now()
-    },
-    ReturnValues: "ALL_NEW"
-  };
+	const data = JSON.parse(event.body);
+	const params = {
+		TableName: process.env.workordersTable,
+		// 'Key' defines the partition key and sort key of the item to be updated
+		// - 'userId': Identity Pool identity id of the authenticated user
+		// - 'workorderId': path parameter
+		Key: {
+			userId: event.requestContext.identity.cognitoIdentityId,
+			workorderId: event.pathParameters.id
+		},
+		// 'UpdateExpression' defines the attributes to be updated
+		// 'ExpressionAttributeValues' defines the value in the update expression
+		UpdateExpression: `SET 
+			title = :title, 
+			clientId = :clientId, 
+			contactId = :contactId, 
+			description = :description, 
+			modifiedAt = :modifiedAt`,
+		ExpressionAttributeValues: {
+			":title": data.title || null,
+			":clientId": data.clientId || null,
+			":contactId": data.contactId || null,
+			":description": data.description || null,
+			":modifiedAt": Date.now()
+		},
+		ReturnValues: "ALL_NEW"
+	};
 
-  try {
-    await dynamoDbLib.call("update", params);
-    return success({ status: true });
-  } catch (e) {
-    return failure({ status: false });
-  }
+	try {
+		await dynamoDbLib.call("update", params);
+		return success({ status: true });
+	} catch (e) {
+		return failure({ status: false });
+	}
 }
