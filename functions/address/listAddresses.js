57ddce5c651b9170bbe1efb4f82f52ae4ee6c2e6
mathewgries <mathewgries@gmail.com>
2019-05-02 19:32:29 -0400

    Because I keep screwing up

--- functions/address/listAddresses.js
+++ functions/address/listAddresses.js
@@ -2,12 +2,9 @@ import * as dynamodbLib from '../../libs/dynamodb-lib'
 import { success, failure } from '../../libs/response-lib'
 
 export async function main(event, context){
-    const data = JSON.parse(event.body)
-    const { content } = data
-
     const params = {
         TableName: process.env.workordersAddressTable,
-        KeyConditionExpression: "relationId = :relationId",
+        KeyConditionExpression: "userId = :userId",
         ExpressionAttributeValues: {
 			":userId": event.requestContext.identity.cognitoIdentityId
 		}
--- functions/phonenumbers/listPhonenumbers.js
+++ functions/phonenumbers/listPhonenumbers.js
@@ -2,9 +2,6 @@ import * as dynamodbLib from '../../libs/dynamodb-lib'
 import { success, failure } from '../../libs/response-lib'
 
 export async function main(event, context){
-    const data = JSON.parse(event.body)
-    const { content } = data
-
     const params = {
         TableName: process.env.workordersPhonenumberTable,
         KeyConditionExpression: "userId = :userId",
